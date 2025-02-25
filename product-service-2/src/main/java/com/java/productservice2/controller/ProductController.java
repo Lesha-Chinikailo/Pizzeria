c4f9b7c41ee59d54d2d958be0e8a82c1272aecc1
Lesha-Chinikailo <lesha2004.0000@gmail.com>
2025-02-25 11:10:17 +0300

    changed path in controller

--- product-service-2/src/main/java/com/java/productservice2/controller/ProductController.java
+++ product-service-2/src/main/java/com/java/productservice2/controller/ProductController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @Validated
+@RequestMapping("api/v1/products")
 public class ProductController {
 
     private final ProductService productService;
