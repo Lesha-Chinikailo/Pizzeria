0502c236115584390a4b30f5dc6df077a75e77a8
Lesha-Chinikailo <lesha2004.0000@gmail.com>
2025-02-26 11:29:52 +0300

    moved error messages to util class

--- product-service-2/src/main/java/com/java/productservice2/service/ProductService.java
+++ product-service-2/src/main/java/com/java/productservice2/service/ProductService.java
@@ -6,6 +6,7 @@ import com.java.productservice2.entity.Product;
 import com.java.productservice2.exception.ProductNotFoundException;
 import com.java.productservice2.mapper.ProductMapper;
 import com.java.productservice2.repository.ProductRepository;
+import com.java.productservice2.util.MessageExceptionUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -28,7 +29,7 @@ public class ProductService {
     public ProductResponse getProductById(Long id) {
         return productMapper.productToProductResponse(
                 productRepository.findById(id)
-                        .orElseThrow(() -> new ProductNotFoundException("Unable to find product with id: " + id)));
+                        .orElseThrow(() -> new ProductNotFoundException(MessageExceptionUtil.UnableFindProductById.formatted(id))));
     }
 
     public List<ProductResponse> getAllProducts() {
@@ -47,7 +48,7 @@ public class ProductService {
 
     public ProductResponse updateProduct(Long id, ProductRequest productRequest) {
         Product product = productRepository.findById(id)
-                .orElseThrow(() -> new ProductNotFoundException("Unable to find product with id: " + id));
+                .orElseThrow(() -> new ProductNotFoundException(MessageExceptionUtil.UnableFindProductById.formatted(id)));
 
         product.setName(productRequest.getName());
         product.setPrice(productRequest.getPrice());
@@ -59,7 +60,7 @@ public class ProductService {
 
     public void deleteProduct(Long id) {
         if(!productRepository.existsById(id)) {
-            throw new ProductNotFoundException("Unable to find product with id: " + id);
+            throw new ProductNotFoundException(MessageExceptionUtil.UnableFindProductById.formatted(id));
         }
         productRepository.deleteById(id);
     }
