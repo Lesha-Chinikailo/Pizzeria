d2449fc65abb8e7e21d62e64b633d346756043cd
Lesha-Chinikailo <lesha2004.0000@gmail.com>
2025-02-27 11:56:15 +0300

    added Messages Exception Util

---
+++ order-service/src/main/java/com/java/orderservice/util/MessageExceptionUtil.java
@@ -0,0 +1,8 @@
+package com.java.orderservice.util;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class MessageExceptionUtil {
+    public static final String UnableFindOrderById = "Unable to find order with id: %s";
+}
