8b5e8abebeae5a794ca2482ef9c4bc992f37c67c
Lesha-Chinikailo <lesha2004.0000@gmail.com>
2025-02-27 12:01:08 +0300

    added global exception handler

---
+++ order-service/src/main/java/com/java/orderservice/handler/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.java.orderservice.handler;
+
+import com.java.orderservice.exception.OrderNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice()
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler({OrderNotFoundException.class})
+    public ResponseEntity<Object> handleOrderNotFoundException(OrderNotFoundException exception) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(exception.getMessage());
+    }
+
+    @ExceptionHandler({RuntimeException.class})
+    public ResponseEntity<Object> handleRuntimeException(RuntimeException exception) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(exception.getMessage());
+    }
+}
