fb634034e56bd080aa1c029863c94d04d8ee7225
Lesha-Chinikailo <lesha2004.0000@gmail.com>
2025-03-05 11:19:21 +0300

    clean up code

--- product-service-2/src/main/java/com/java/productservice2/config/AuthConfig.java
+++ product-service-2/src/main/java/com/java/productservice2/config/AuthConfig.java
@@ -1,12 +1,10 @@
 package com.java.productservice2.config;
 
 import com.java.productservice2.filter.JwtAuthenticationFilter;
-import com.java.productservice2.service.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -14,27 +12,12 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
-//@RequiredArgsConstructor
-//@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class AuthConfig {
-
-    @Bean("customUserDetailsService")
-    public UserDetailsService getUserDetailsService() {
-        return new CustomUserDetailsService();
-    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http.csrf(csrf -> csrf.disable());
-//        http.authorizeHttpRequests(authorize -> authorize
-//                .anyRequest().authenticated());
-//        return http.build();
-//    }
 
     @Bean
     public JwtAuthenticationFilter authenticationTokenFilterBean() throws Exception {
